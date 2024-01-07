5300bd4f48a3b19084aeb5839457071b568463f7
Crowdbotics <team@crowdbotics.com>
2024-01-07 23:27:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-poetry-45310",
+  url: "https://yellow-poetry-45310.botics.co",
+  api: "https://yellow-poetry-45310.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
